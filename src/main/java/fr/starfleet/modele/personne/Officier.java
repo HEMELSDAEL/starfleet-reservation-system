ad543d1d628c856da975318cffadd56569f6d03c
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-03-21 13:09:01 +0100

    Ajout de la javadoc de la classe Officier

--- src/main/java/fr/starfleet/modele/personne/Officier.java
+++ src/main/java/fr/starfleet/modele/personne/Officier.java
@@ -1,31 +1,62 @@
 package starfleet_reservation_system.src.main.java.fr.starfleet.modele.personne;
 
+/**Classe Officer qui hérite de la classe Personne */
 public class Officier extends Personne{
+    /**Attribut rang de l'officier */
     private String rang;
+    /**Attribut specialite de l'officer */
     private String specialite;
 
+    /**
+     * Constructeur qui permet de créer un officier
+     * @param nom : nom de l'officier
+     * @param prenom : prenom de l'officier
+     * @param identifiant : identifiant de l'officier
+     * @param rang : rang de l'officier
+     * @param specialite : specialite de l'officer
+     */
     public Officier(String nom, String prenom, String identifiant, String rang, String specialite){
         super(nom, prenom, identifiant);
         this.rang = rang;
         this.specialite = specialite;
     }
 
+    /**
+     * Getter qui recupère le rang de l'officier
+     * @return : le rang de l'officer
+     */
     public String getRang(){
         return this.rang;
     }
 
+    /**
+     * Setter qui permet de modifier le rang de l'officier
+     * @param newRang : nouveau rang de l'officier
+     */
     public void setRang(String newRang){
         this.rang = newRang;
     }
 
+    /**
+     * Getter qui permet de récupérer la spécialité de l'officier
+     * @return : la spécialité de l'officier
+     */
     public String getSpecialite(){
         return this.specialite;
     }
 
+    /**
+     * Setter qui permet de changer la spécialité de l'officier
+     * @param newSpecialite : nouvelle spécialité de l'officier
+     */
     public void setSpecialite(String newSpecialite){
         this.specialite = newSpecialite;
     }
 
+    /**
+     * Méthode qui permet d'afficher une description détaillée de l'officier
+     * @return : une description détaillée de l'officier
+     */
     @Override
     public String getDescription(){
         return "Cet officier s'appelle : " + super.getNom() + " , " + super.getPrenom() + " son rang est : " + this.getRang() + " et sa spécialité est : " + this.getSpecialite();
