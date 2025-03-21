ea2759383008605c3d7ec78e7732c0f8057df2d0
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-03-21 15:27:59 +0100

    ajout du début de la javadoc de la classe Reservation

--- src/main/java/fr/starfleet/modele/reservation/Reservation.java
+++ src/main/java/fr/starfleet/modele/reservation/Reservation.java
@@ -5,13 +5,27 @@ import java.util.*;
 import starfleet_reservation_system.src.main.java.fr.starfleet.modele.mission.Mission;
 import starfleet_reservation_system.src.main.java.fr.starfleet.modele.personne.Personne;
 
+/**Classe réservation */
 public class Reservation {
+    /**Attribut idReservation de la classe reservation */
     private String idReservation;
+    /**Attribut personne de la classe */
     private Personne personne;
+    /**Attribut mission de la classe */
     private Mission mission;
+    /**Attribut dateReservation de la classe */
     private Date dateReservation;
+    /**Attribut confirmee de la classe */
     private boolean confirmee;
 
+    /**
+     * Constructeur qui permet de créer une réservation
+     * @param idReservation : id de la réservation
+     * @param personne : personne qui a réservé
+     * @param mission : mission pour laquelle il a réservé
+     * @param dateReservation : date de la réservation
+     * @param confirmee : confirmation de la réservation
+     */
     public Reservation(String idReservation, Personne personne, Mission mission, Date dateReservation, boolean confirmee){
         this.idReservation = idReservation;
         this.personne = personne;
@@ -20,22 +34,42 @@ public class Reservation {
         this.confirmee = confirmee;
     }
 
+    /**
+     * Getter qui récupère l'id de la réservation 
+     * @return : l'id de la réservation
+     */
     public String getIdReservation(){
         return this.idReservation;
     }
 
+    /**
+     * Setter qui modifie l'id de réservation 
+     * @param idReservation : nouvel id de réservation
+     */
     public void setIdReservation(String idReservation){
         this.idReservation = idReservation;
     }
 
+    /**
+     * Getter qui récupère la personne qui a fait la réservation
+     * @return : la personne qui a fait la réservation
+     */
     public Personne GetPersonne(){
         return this.personne;
     }
 
+    /**
+     * Setter qui modifie la personne qui a effectué la réservation
+     * @param newPersonne : nouvelle personne qui effectue la réservation
+     */
     public void setPersonne(Personne newPersonne){
         this.personne = newPersonne;
     }
 
+    /**
+     * Getter qui récupère la mission pour la réservation
+     * @return : la mission
+     */
     public Mission getMission(){
         return this.mission;
     }
