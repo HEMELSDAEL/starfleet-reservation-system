89b5c891b5164b1a5c5412325f55dd0971f5378e
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-03-21 09:07:14 +0100

    ajout du début de la javadoc de la classe Civil

--- src/main/java/fr/starfleet/modele/personne/Civil.java
+++ src/main/java/fr/starfleet/modele/personne/Civil.java
@@ -1,23 +1,46 @@
 package starfleet_reservation_system.src.main.java.fr.starfleet.modele.personne;
 
+/**Classe Civil qui hérite de la classe Personne */
 public class Civil extends Personne{
+    /**Attribut planeteOrigine du civil */
     private String planeteOrigine;
+    /**Attribut motifVoyage du civil */
     private String motifVoyage;
 
+    /**
+     * Constructeur de la classe Civil pour créer un objet Civil
+     * @param nom : nom du civil
+     * @param prenom : prenom du civil
+     * @param identifiant : identifiant du civil
+     * @param planeteOrigine : planete d'origine du civil
+     * @param motifVoyage : motif du voyage du civil
+     */
     public Civil(String nom, String prenom, String identifiant, String planeteOrigine, String motifVoyage){
         super(nom, prenom, identifiant);
         this.planeteOrigine = planeteOrigine;
         this.motifVoyage = motifVoyage;
     }
 
+    /**
+     * Getter pour récupérer la planète d'origine du civil
+     * @return : la planète d'origine du civil
+     */
     public String getPlaneteOrigine(){
         return this.planeteOrigine;
     }
 
+    /**
+     * Setter pour modifier la planète d'origine du civil
+     * @param newPlanete : nouvelle planète d'origine
+     */
     public void setPlaneteOrigine(String newPlanete){
         this.planeteOrigine = newPlanete;
     }
 
+    /**
+     * Getter pour récupérer le motif du voyage
+     * @return : le motif du voyage du civil
+     */
     public String getMotifVoyage(){
         return this.motifVoyage;
     }
