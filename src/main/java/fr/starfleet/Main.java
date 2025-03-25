a4f60edf6288439d16373e81b55d6a734334ea52
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-03-25 16:58:37 +0100

    ajout de la classe Main

---
+++ src/main/java/fr/starfleet/Main.java
@@ -0,0 +1,11 @@
+package fr.starfleet;
+
+import fr.starfleet.ui.InterfaceConsole;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        InterfaceConsole interfaceConsole = new InterfaceConsole();
+        interfaceConsole.demarrer();
+    }
+}
